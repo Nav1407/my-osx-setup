914f2df47d6dbe049adf3550d11770871b07d6c9
Nav <nkaur@aglet.app>
2024-08-29 08:55:43 +1000

    correcting path to ansible binaries

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 user=`whoami`
-export PATH="$PATH:/opt/homebrew/bin/:/Users/${user}/Library/Application Support/pipx/venvs/ansible/bin/"
+export PATH="$PATH:~/.local/bin"
 
-brew_path=`brew --prefix`
+brew_path=`/opt/homebrew/bin/brew --prefix`
 
 eval "$(/${brew_path}/bin/brew shellenv)"
 
